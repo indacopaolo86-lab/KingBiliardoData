890f6daf354592549e4f1e5ab23241e234bda7ad
Paolo Indaco <indaco.paolo86@email.com>
2025-09-24 22:52:48 +0200

    adesso  funziona  l'app

--- updateLive.js
+++ updateLive.js
@@ -3,21 +3,41 @@ import fetch from "node-fetch";
 
 async function aggiornaLive() {
   const apiKey = "AIzaSyCFeCh0ZtRew3tJO8JfRuqR41YQDxEud2k";
-  const channelId = "Paxy_86";
+  const channelId = "UCaJbcPGyEcpwHHArQhophWw";
+  const tavoli = [];
 
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${apiKey}`;
+  let pageToken = undefined; // inizia senza token
+  do {
+    const url = new URL("https://www.googleapis.com/youtube/v3/search");
+    url.searchParams.set("part", "snippet");
+    url.searchParams.set("channelId", channelId);
+    url.searchParams.set("eventType", "live");
+    url.searchParams.set("type", "video");
+    url.searchParams.set("maxResults", "50");
+    url.searchParams.set("key", apiKey);
+    if (pageToken) {
+      url.searchParams.set("pageToken", pageToken);
+    }
 
-  const res = await fetch(url);
-  const data = await res.json();
+    const res = await fetch(url.toString());
+    const data = await res.json();
 
-  const tavoli = data.items.map((item, index) => ({
-    nome: `Tavolo ${index + 1} - ${item.snippet.title}`,
-    codice: item.id.videoId
-  }));
+    if (!data.items || data.items.length === 0) break;
+
+    data.items.forEach((item) => {
+      tavoli.push({
+        nome: `Tavolo ${tavoli.length + 1} - ${item.snippet.title}`,
+        codice: item.id.videoId
+      });
+    });
+
+    pageToken = data.nextPageToken; // token della pagina successiva
+  } while (pageToken);
 
   const json = { tavoli };
   fs.writeFileSync("streamingurls.json", JSON.stringify(json, null, 2));
   console.log("File streamingurls.json aggiornato!");
+  console.log(json);
 }
 
 aggiornaLive().catch(console.error);
